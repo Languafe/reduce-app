06c7334fe9424c68effaf992ee8434cfa5312651
Erik Langhaug <erik.langhaug@outlook.com>
2019-07-27 21:05:27 +0200

    reduce-app first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    items: [],
+    stats: {},
+    name: ''
+  }
+
+  handleChange = (e) => this.setState({ [e.target.name]: e.target.value });
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const newItem = {
+      id: this.state.items.length + 1,
+      name: this.state.name
+    };
+    const items = [...this.state.items, newItem];
+    const stats = items.reduce((previousValue, currentValue, currentIndex, items) => {
+      previousValue[currentValue.name] 
+        ? previousValue[currentValue.name]++
+        : previousValue[currentValue.name] = 1
+      return previousValue;
+    }, {});
+    this.setState({ items, stats, name: '' });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>reduce-app</h1>
+        <form onSubmit={this.handleSubmit} autoComplete="off">
+          <input autoFocus type="text" name="name" value={this.state.name} placeholder="name something..." onChange={this.handleChange} />
+          <button type="submit">Save</button>
+        </form>
+        <hr />
+        <h2>Items</h2>
+        <ul>
+          {this.state.items.map(item => 
+            <li key={item.id}>{item.name}</li>
+          )}
+        </ul>
+        <hr />
+        <h2>Stats</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>Word</th>
+              <th>Count</th>
+            </tr>
+          </thead>
+          <tbody>
+            {Object.keys(this.state.stats).map((name, i) =>
+              <tr key={i}>
+                <td>{name}</td>
+                <td>{this.state.stats[name]}</td>
+              </tr>  
+            )}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default App;
